396030960a6ebe83b99c3a5b1f55963fe615d266
Tomonori Ishizaki <tomishi.android@gmail.com>
2016-01-09 11:10:09 +0900

    Create brank main activity

---
+++ app/src/main/java/com/tomishi/sampletvapp/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tomishi.sampletvapp.ui;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.tomishi.sampletvapp.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
