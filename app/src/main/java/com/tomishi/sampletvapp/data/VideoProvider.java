678e053ccccf5d93da933aa9a79ba48eddfd3bd4
Tomonori Ishizaki <tomishi.android@gmail.com>
2016-01-11 17:42:06 +0900

    Set actual information in video item

--- app/src/main/java/com/tomishi/sampletvapp/data/VideoProvider.java
+++ app/src/main/java/com/tomishi/sampletvapp/data/VideoProvider.java
@@ -28,17 +28,17 @@ public class VideoProvider {
                     Video video = new Video();
                     video.setId(0);
                     video.setResourceId(R.raw.video1);
-                    video.setTitle("Giraffe");
-                    video.setStudio("free video");
-                    video.setDescription("");
+                    video.setTitle("キリン　夕日を背景に");
+                    video.setStudio("ＮＨＫクリエイティブ・ライブラリ");
+                    video.setDescription("アフリカ南部、ナミビアにあるナミブ砂漠。夕日が沈もうとしている。夕日を背景に、親子のキリンが移動中。薄暮の時間は、気温も下がり、つかの間の平和なひととき。２００４年１１月から１２月に撮影。");
                     add(video);
 
                     video = new Video();
                     video.setId(1);
                     video.setResourceId(R.raw.video2);
-                    video.setTitle("Dinosaur");
-                    video.setStudio("free video");
-                    video.setDescription("");
+                    video.setTitle("ティラノサウルスとデイノスクスに追い詰められたクリトサウルス");
+                    video.setStudio("ＮＨＫクリエイティブ・ライブラリ");
+                    video.setDescription("中生代白亜紀の水辺で展開されていた恐竜やワニの祖先の捕食の様子を再現したＣＧ。クリトサウルスという中型の植物食恐竜が大形肉食恐竜ティラノサウルスに水辺まで追い詰められている。突然、背後の水中から出現した大型のワニであるデイノスクスがクリトサウルスを捕食してしまう様子。デイノスクスは、同時期の北アメリカに生息していたた史上最大級のワニ。");
                     add(video);
                 }
             }
