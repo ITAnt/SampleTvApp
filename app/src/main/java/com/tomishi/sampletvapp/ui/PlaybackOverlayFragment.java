48ed5ebf43228f97d6969b78ed46c21184232922
Tomonori Ishizaki <tomishi.android@gmail.com>
2016-01-11 22:54:12 +0900

    Add PlayControlsRow in PlaybackOverlayFragment

--- app/src/main/java/com/tomishi/sampletvapp/ui/PlaybackOverlayFragment.java
+++ app/src/main/java/com/tomishi/sampletvapp/ui/PlaybackOverlayFragment.java
@@ -1,14 +1,38 @@
 package com.tomishi.sampletvapp.ui;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v17.leanback.widget.ArrayObjectAdapter;
+import android.support.v17.leanback.widget.ClassPresenterSelector;
+import android.support.v17.leanback.widget.ControlButtonPresenterSelector;
+import android.support.v17.leanback.widget.ListRow;
+import android.support.v17.leanback.widget.ListRowPresenter;
+import android.support.v17.leanback.widget.PlaybackControlsRow;
+import android.support.v17.leanback.widget.PlaybackControlsRowPresenter;
 import android.util.Log;
 
 import com.tomishi.sampletvapp.model.Video;
+import com.tomishi.sampletvapp.presenter.VideoDetailsDescriptionPresenter;
 
 public class PlaybackOverlayFragment extends android.support.v17.leanback.app.PlaybackOverlayFragment {
 
     private static final String TAG = PlaybackOverlayFragment.class.getSimpleName();
     private Video mSelectedVideo;
+    private ArrayObjectAdapter mRowsAdapter;
+    private ArrayObjectAdapter mPrimaryActionsAdapter;
+    private ArrayObjectAdapter mSecondaryActionsAdapter;
+    private PlaybackControlsRow.PlayPauseAction mPlayPauseAction;
+    private PlaybackControlsRow.RepeatAction mRepeatAction;
+    private PlaybackControlsRow.ThumbsUpAction mThumbsUpAction;
+    private PlaybackControlsRow.ThumbsDownAction mThumbsDownAction;
+    private PlaybackControlsRow.ShuffleAction mShuffleAction;
+    private PlaybackControlsRow.SkipNextAction mSkipNextAction;
+    private PlaybackControlsRow.SkipPreviousAction mSkipPreviousAction;
+    private PlaybackControlsRow.FastForwardAction mFastForwardAction;
+    private PlaybackControlsRow.RewindAction mRewindAction;
+    private PlaybackControlsRow.HighQualityAction mHighQualityAction;
+    private PlaybackControlsRow.ClosedCaptioningAction mClosedCaptioningAction;
+    private PlaybackControlsRow.MoreActions mMoreActions;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -16,5 +40,67 @@ public class PlaybackOverlayFragment extends android.support.v17.leanback.app.Pl
         super.onCreate(savedInstanceState);
 
         mSelectedVideo = (Video) getActivity().getIntent().getSerializableExtra(VideoDetailsActivity.VIDEO);
+
+        setBackgroundType(PlaybackOverlayFragment.BG_LIGHT);
+        setFadingEnabled(true);
+
+        setUpRows();
+    }
+
+    private void setUpRows() {
+        // create adapter
+        ClassPresenterSelector ps = new ClassPresenterSelector();
+
+        PlaybackControlsRowPresenter playbackControlsRowPresenter;
+        playbackControlsRowPresenter = new PlaybackControlsRowPresenter(new VideoDetailsDescriptionPresenter());
+
+        ps.addClassPresenter(PlaybackControlsRow.class, playbackControlsRowPresenter);
+        ps.addClassPresenter(ListRow.class, new ListRowPresenter());
+        mRowsAdapter = new ArrayObjectAdapter(ps);
+
+        addPlaybackControlsRow();
+
+        setAdapter(mRowsAdapter);
+    }
+
+    private void addPlaybackControlsRow() {
+        PlaybackControlsRow playbackControlsRow = new PlaybackControlsRow(mSelectedVideo);
+        mRowsAdapter.add(playbackControlsRow);
+
+        ControlButtonPresenterSelector presenterSelector = new ControlButtonPresenterSelector();
+        mPrimaryActionsAdapter = new ArrayObjectAdapter(presenterSelector);
+        mSecondaryActionsAdapter = new ArrayObjectAdapter(presenterSelector);
+        playbackControlsRow.setPrimaryActionsAdapter(mPrimaryActionsAdapter);
+        playbackControlsRow.setSecondaryActionsAdapter(mSecondaryActionsAdapter);
+
+        Activity activity = getActivity();
+        mPlayPauseAction = new PlaybackControlsRow.PlayPauseAction(activity);
+        mRepeatAction = new PlaybackControlsRow.RepeatAction(activity);
+        mThumbsUpAction = new PlaybackControlsRow.ThumbsUpAction(activity);
+        mThumbsDownAction = new PlaybackControlsRow.ThumbsDownAction(activity);
+        mShuffleAction = new PlaybackControlsRow.ShuffleAction(activity);
+        mSkipNextAction = new PlaybackControlsRow.SkipNextAction(activity);
+        mSkipPreviousAction = new PlaybackControlsRow.SkipPreviousAction(activity);
+        mFastForwardAction = new PlaybackControlsRow.FastForwardAction(activity);
+        mRewindAction = new PlaybackControlsRow.RewindAction(activity);
+        mHighQualityAction = new PlaybackControlsRow.HighQualityAction(activity);
+        mClosedCaptioningAction = new PlaybackControlsRow.ClosedCaptioningAction(activity);
+        mMoreActions = new PlaybackControlsRow.MoreActions(activity);
+
+        // setup PrimaryAction
+        mPrimaryActionsAdapter.add(mSkipPreviousAction);
+        mPrimaryActionsAdapter.add(mRewindAction);
+        mPrimaryActionsAdapter.add(mPlayPauseAction);
+        mPrimaryActionsAdapter.add(mFastForwardAction);
+        mPrimaryActionsAdapter.add(mSkipNextAction);
+
+        // setup SecondaryAction
+        mSecondaryActionsAdapter.add(mThumbsUpAction);
+        mSecondaryActionsAdapter.add(mThumbsDownAction);
+        mSecondaryActionsAdapter.add(mRepeatAction);
+        mSecondaryActionsAdapter.add(mShuffleAction);
+        mSecondaryActionsAdapter.add(mHighQualityAction);
+        mSecondaryActionsAdapter.add(mClosedCaptioningAction);
+        mSecondaryActionsAdapter.add(mMoreActions);
     }
 }
